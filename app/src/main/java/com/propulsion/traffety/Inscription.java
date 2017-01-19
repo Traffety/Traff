09a6fc40f2b4ace1c8d13872c8db4cee9ccf6139
eric <ericzile90@gmail.com>
2017-01-19 11:11:47 +0000

    Traffety Init
    Traffety is the safety monitoring mobile app letting users know anytime
    where is their network for emergency services required

---
+++ app/src/main/java/com/propulsion/traffety/Inscription.java
@@ -0,0 +1,13 @@
+package com.propulsion.traffety;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Inscription extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription);
+    }
+}
